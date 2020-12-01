81732c58d05e77febb61b76147c9280ac2abff47
krishna crypt <kvenk@outlook.com>
2020-12-01 16:52:45 -0500

    intial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,36 @@
+class HomeController < ApplicationController
+  def index
+  	require 'net/http'
+  	require 'json'
+  	# Grab News Data 
+  	@url = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
+  	@uri = URI(@url)
+   	@response = Net::HTTP.get(@uri)
+  	@news = JSON.parse(@response)
+
+  	# Grab Price Data
+  	@prices_url = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XLM,MIOTA,USDT,TRX&tsyms=USD,EUR"
+  	@prices_uri = URI(@prices_url)
+   	@prices_response = Net::HTTP.get(@prices_uri)
+  	@prices = JSON.parse(@prices_response)
+  end
+
+  def prices
+    # Grab Price Data 
+    require 'net/http'
+    require 'json'
+
+    @symbol = params[:sym]
+
+    if @symbol
+        @symbol=@symbol.upcase
+    
+         # Grab Price Data
+        @quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + @symbol + '&tsyms=USD'
+        @quote_uri = URI(@quote_url)
+        @quote_response = Net::HTTP.get(@quote_uri)
+        @quote = JSON.parse(@quote_response)
+    end
+ 
+  end
+end
